7da027938b59db2c00cd60bf16cbde99fdb2a686
Ulises Santana <ulisesantana@gmail.com>
2017-10-15 18:57:36 +0100

    Basic generator

---
+++ generators/app/index.js
@@ -0,0 +1,57 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the sublime ' + chalk.red('generator-express-ts2') + ' generator!'
+    ));
+
+    // const prompts = [{
+    //   type: 'confirm',
+    //   name: 'someAnswer',
+    //   message: 'Would you like to enable this option?',
+    //   default: true
+    // }];
+
+    // return this.prompt(prompts).then(props => {
+    //   // To access props later use this.props.someAnswer;
+    //   this.props = props;
+    // });
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js')
+    );
+    this.fs.copy(
+      this.templatePath('tsconfig.json'),
+      this.destinationPath('tsconfig.json')
+    );
+    this.fs.copy(
+      this.templatePath('package-lock.json'),
+      this.destinationPath('package-lock.json')
+    );
+    this.fs.copy(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json')
+    );
+    this.fs.copy(
+      this.templatePath('src/index.ts'),
+      this.destinationPath('src/index.ts')
+    );
+  }
+
+  install() {
+    console.log(git.user);
+    this.installDependencies({
+      npm: true,
+      bower: false,
+      yarn: false
+    });
+  }
+};
