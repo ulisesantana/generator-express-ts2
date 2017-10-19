a25c4c6b5e94dea3b0aafd7d47dbef3e7949590e
Ulises Santana <ulisesantana@gmail.com>
2017-10-19 21:25:42 +0100

    Added basic index file

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,1 +1,9 @@
+import * as os from 'os';
 
+console.log(`HOSTNAME: ${os.hostname}`);
+console.log(`PLATFORM: ${os.platform}`);
+console.log(`RELEASE: ${os.release}`);
+console.log(`CPUS: ${os.cpus().length}x ${JSON.stringify(os.cpus()[0].model)}`);
+console.log(`HOME DIRECTORY: ${os.homedir}`);
+console.log(`FREE MEMORY: ${(os.freemem / Number("1E9")).toFixed(2)}GB`);
+console.log(`TOTAL MEMORY: ${(os.totalmem / Number("1E9")).toFixed(2)}GB`);
